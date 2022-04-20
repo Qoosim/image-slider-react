ebfd6003d50f53fdd7d16e41f8f8665ca3ed36dd
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-04-20 12:17:20 +0100

    Add logic to slide through all the images in the data.js file whenever either of the right or left arrow is clicked. setInterval method is also used to automatically slide the images at every 3 seconds

--- src/components/People.js
+++ src/components/People.js
@@ -1,15 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 import PersonDetails from './PersonDetails';
 import data from '../data';
 
 const People = () => {
+  const [people] = useState(data);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    let lastIndex = people.length - 1;
+
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index - 1);
+    }, 3000)
+
+    return (() => clearInterval(slider));
+
+  }, [index]);
+
   return (
     <div className="section-center">
       {
-        data.map((person) => {
-          return <PersonDetails key={person.id} person={person} />
+        data.map((person, personIndex) => {
+          let position = 'nextSlide';
+
+          if (personIndex === index) {
+            position = 'activeSlide';
+          }
+
+          if (personIndex === index - 1 || (index === 0 && personIndex === people.length - 1)) {
+            position = 'lastSlide';
+          }
+
+          return <PersonDetails key={person.id} person={person} position={position} />
         })
       }
+      <button className="prev" onClick={() => setIndex(index - 1)}><FiChevronLeft /></button>
+      <button className="next" onClick={() => setIndex(index + 1)}><FiChevronRight /></button>
     </div>
   )
 }
--- src/components/PersonDetails.js
+++ src/components/PersonDetails.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { FaQuoteRight } from 'react-icons/fa';
 
-const PersonDetails = ({ person }) => {
+const PersonDetails = ({ person, position }) => {
   const { id, name, image, title, quote } = person;
 
   return (
-    <article>
-
+    <article key={id} className={position}>
+      <img src={image} alt={name} className="person-img" />
+      <h4>{name}</h4>
+      <p className="title">{title}</p>
+      <p className="text">{quote}</p>
+      <FaQuoteRight className="icon" />
     </article>
   )
 }
